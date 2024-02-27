ee6c1920a2a426d7e5b539e169894223fd7ea867
spip <raise_issue>
2024-02-27 13:07:23 +0100

    Added nick

---
+++ challenges/programming-misc/eye-doctor/solve/writeups-chat/solve_Almandin.py
@@ -0,0 +1,9 @@
+Solve by C500G and Almandin from team CyberLimoux
+ Using Gimp - posted in writeups chat
+
+- Open "The GIMP"
+- Click filter > distorsion > relief
+- Change azimut to aproximatly 193.45
+- Change elevation to aproximatly 23.82
+- Change depth to aproximatly 8
+- The flag is clear and easily readable
